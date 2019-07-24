b2fe14e168381a4e84aea84e312864f605faea8f
eliothowes1090 <ejhowes@gmail.com>
2019-07-24 18:04:58 +0100

    Created bears table and queries to collect data

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+    VALUES ("Mr. Chocolate", 12, "Male", "Brown", "Calm", 1),
+    ("Rowdy", 4, "Male", "Brown", "Calm", 1),
+    ("Tabitha", 9, "Female", "Brown", "Calm", 1),
+    ("Sergeant Brown", 21, "Male", "Brown", "Calm", 1),
+    ("Melissa", 15, "Female", "Brown", "Calm", 1),
+    ("Grinch", 1, "Male", "Brown", "Calm", 1),
+    ("Wendy", 8, "Female", "Brown", "Calm", 1),
+    (NULL, 11, "Male", "Brown", "Quiet", 0);
