226f579216ec19ae3887336481d2fe874670113a
github username <github email address>
2019-07-24 14:16:52 +0100

    lab broken

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Rowdy', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Tabitha', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Sergeant Brown', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Melissa', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Grinch', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Wendy', 7, 'male', 'brown', 'friendly', 'alive');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES (null, 7, 'male', 'brown', 'friendly', 'alive');
